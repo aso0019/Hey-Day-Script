b4843feec475fa26c64f81652cfd4abbb18562ca
aso0019 <146109881+aso0019@users.noreply.github.com>
2023-10-14 20:35:23 -0500

    Add files via upload

---
+++ HeyDayScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Prompt the user to enter their name
+read -p "Kindly enter your name: " username
+
+# Greet the user Hey day by putting Hey in front of their name
+echo "Hey, $username!"
+
